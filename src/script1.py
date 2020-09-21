a4a5c30092c1507aa73ea67fedc31a37b6cbb46c
Mika-IO <mikaio4321@gmail.com>
2020-09-21 12:29:14 -0400

    some readme modifications

--- src/script1.py
+++ src/script1.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 import requests
-import os
 import sys
 from tqdm import tqdm
 
--- src/script2.py
+++ src/script2.py
@@ -1,3 +1,8 @@
+# coding: utf-8
+import requests
+import sys
+from tqdm import tqdm
+
 def read_data(directory, period, *args):
     pass
 
